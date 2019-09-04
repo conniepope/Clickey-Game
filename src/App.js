b0541f5c03c272e8d2495579fdb2b2ff67309e80
conniepope <conniepope77@gmail.com>
2019-09-04 12:45:17 -0400

    file setup

---
+++ src/App.js
@@ -0,0 +1,38 @@
+import React, { Component } from "react";
+import Card from "./components/Card";
+import Wrapper from "./components/Wrapper";
+import Header from "./components/Header";
+import characters from "./characters.json";
+
+class App extends Component {
+  // Setting this.state.friends to the friends json array
+  state = {
+    characters
+  };
+
+  // removeFriend = id => {
+  //   // Filter this.state.friends for friends with an id not equal to the id being removed
+  //   const friends = this.state.friends.filter(friend => friend.id !== id);
+  //   // Set this.state.friends equal to the new friends array
+  //   this.setState({ friends });
+  // };
+
+  // Map over this.state.friends and render a FriendCard component for each friend object
+  render() {
+    return (
+      <Wrapper>
+        <Header>Disney Characters</Header>
+        {this.state.characters.map(character => (
+          <Card
+            id={character.id}
+            key={character.id}
+            name={character.name}
+            image={character.image}
+          />
+        ))}
+      </Wrapper>
+    );
+  }
+}
+
+export default App;
