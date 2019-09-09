bb659625af134ec287e8bebe1e51762ad2ac294c
conniepope <conniepope77@gmail.com>
2019-09-09 18:36:27 -0400

    Updates

---
+++ precache-manifest.0cee3280fb0d38a2a9e9b38a6deb6c40.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5bba9601a4e65b8fc15b0c7b13192304",
+    "url": "/conniepope/Clickey-Game/index.html"
+  },
+  {
+    "revision": "c183b7a4006d98507130",
+    "url": "/conniepope/Clickey-Game/static/css/main.e09576af.chunk.css"
+  },
+  {
+    "revision": "36ff4c138fb4ac3c0a4b",
+    "url": "/conniepope/Clickey-Game/static/js/2.333e8cac.chunk.js"
+  },
+  {
+    "revision": "c183b7a4006d98507130",
+    "url": "/conniepope/Clickey-Game/static/js/main.c9eed5c2.chunk.js"
+  },
+  {
+    "revision": "fd39f2d9ca17805a6ca3",
+    "url": "/conniepope/Clickey-Game/static/js/runtime~main.d405668d.js"
+  },
+  {
+    "revision": "328356c9fef5c35ec740870573e192c6",
+    "url": "/conniepope/Clickey-Game/static/media/peeking-mickey.328356c9.jpg"
+  }
+]);
